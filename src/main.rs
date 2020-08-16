a4c9da006d9f8d99701f5e14222cc4772c14ae08
Joshua Benuck <joshua_benuck@yahoo.com>
2020-08-16 16:39:28 -0700

    create map abstraction

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod player;
 mod rect;
 
 use components::{Player, Position, Renderable};
-use map::{draw_map, new_map_rooms_and_cooridors, xy_idx, TileType};
+use map::{draw_map, Map, TileType};
 use player::player_input;
 use rect::Rect;
 use rltk::{GameState, Rltk, RGB};
@@ -27,8 +27,8 @@ impl GameState for State {
         player_input(self, ctx);
         self.run_systems();
 
-        let map = self.ecs.fetch::<Vec<TileType>>();
-        draw_map(&map, ctx);
+        let map = self.ecs.fetch::<Map>();
+        draw_map(&map.tiles, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -53,9 +53,9 @@ fn main() {
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
 
-    let (rooms, map) = new_map_rooms_and_cooridors();
+    let map = Map::new_map_rooms_and_cooridors();
+    let (player_x, player_y) = map.rooms[0].center();
     gs.ecs.insert(map);
-    let (player_x, player_y) = rooms[0].center();
 
     gs.ecs
         .create_entity()
--- src/player.rs
+++ src/player.rs
@@ -1,4 +1,4 @@
-use crate::{xy_idx, Player, Position, State, TileType};
+use crate::{Map, Player, Position, State, TileType};
 use rltk::{Rltk, VirtualKeyCode};
 use specs::prelude::*;
 use std::cmp::{max, min};
@@ -6,12 +6,13 @@ use std::cmp::{max, min};
 pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TileType>>();
+    let map = ecs.fetch::<Map>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
         let dest_x = min(79, max(0, pos.x + delta_x));
         let dest_y = min(49, max(0, pos.y + delta_y));
-        if map[xy_idx(dest_x, dest_y)] != TileType::Wall {
+        let dest_idx = map.xy_idx(dest_x, dest_y);
+        if map.tiles[dest_idx] != TileType::Wall {
             pos.x = dest_x;
             pos.y = dest_y;
         }
