86b73293b153644ee0d957ea6daeb35941de3ba2
Joshua Benuck <joshua_benuck@yahoo.com>
2020-08-16 19:36:41 -0700

    add map memory, speed up rendering

--- src/visibility_system.rs
+++ src/visibility_system.rs
@@ -1,4 +1,4 @@
-use crate::{Map, Position, Viewshed};
+use crate::{Map, Player, Position, Viewshed};
 use rltk::{field_of_view, Point};
 use specs::prelude::*;
 
@@ -6,19 +6,29 @@ pub struct VisibilitySystem {}
 
 impl<'a> System<'a> for VisibilitySystem {
     type SystemData = (
-        ReadExpect<'a, Map>,
+        WriteExpect<'a, Map>,
+        Entities<'a>,
         WriteStorage<'a, Viewshed>,
         WriteStorage<'a, Position>,
+        ReadStorage<'a, Player>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (map, mut viewshed, pos) = data;
-        for (viewshed, pos) in (&mut viewshed, &pos).join() {
+        let (mut map, entities, mut viewshed, pos, player) = data;
+        for (ent, viewshed, pos) in (&entities, &mut viewshed, &pos).join() {
             viewshed.visible_tiles.clear();
             viewshed.visible_tiles = field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
             viewshed
                 .visible_tiles
                 .retain(|p| p.x > 0 && p.x < map.width - 1 && p.y > 0 && p.y < map.height - 1);
+
+            let p: Option<&Player> = player.get(ent);
+            if let Some(p) = p {
+                for vis in viewshed.visible_tiles.iter() {
+                    let idx = map.xy_idx(vis.x, vis.y);
+                    map.revealed_tiles[idx] = true;
+                }
+            }
         }
     }
 }
